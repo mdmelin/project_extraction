8fab9fd208541d583638b3ff3ceef2f8f0fc7636
olivier <olivier.winter@hotmail.fr>
2023-02-10 11:41:22 +0000

    add exit state as key in the trials table for synchronisation

--- examples/neuromodulators.py
+++ examples/neuromodulators.py
@@ -1,6 +1,14 @@
 ## %% Example of a trials table extraction for Neuromodulator trials
 from projects.neuromodulators import TrialsTableNeuromodulator
 
-session_path = "/home/olivier/Downloads/D6/2023-02-08/001"
+
+session_path = "/datadisk/gdrive/2023/02_Neuromodulators/D6/2023-02-08/001"
 extractor = TrialsTableNeuromodulator(session_path)
-extractor.extract(save=True)
+_, data_files = extractor.extract(save=True)
+
+
+## %% Loads the data afterwards
+import pandas as pd
+trials = pd.read_parquet(data_files[0])
+
+assert set(['exit_state', 'omit_feedback']).issubset(set(trials.keys()))
--- projects/neuromodulators.py
+++ projects/neuromodulators.py
@@ -7,4 +7,5 @@ class TrialsTableNeuromodulator(TrialsTableBiased):
     def _extract(self, *args, **kwargs):
         out = super(TrialsTableNeuromodulator, self)._extract(*args, **kwargs)
         out[0]['omit_feedback'] = np.array([t['omit_feedback'] for t in self.bpod_trials])
+        out[0]['exit_state'] = np.array([t['behavior_data']['States timestamps']['exit_state'][0][0] for t in self.bpod_trials])
         return out
