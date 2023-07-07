d4fb0b71eb8da965e4dc64785deb19911352fad7
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2023-07-07 12:50:43 +0100

    get daq data

--- projects/biased_fibrephotometry.py
+++ projects/biased_fibrephotometry.py
@@ -12,34 +12,13 @@ from iblutil.util import Bunch
 
 from ibllib.pipes import tasks
 from ibllib.io.extractors.fibrephotometry import FibrePhotometry as BaseFibrePhotometry
-from ibllib.io.extractors.fibrephotometry import DAQ_CHMAP
+from ibllib.io.extractors.fibrephotometry import DAQ_CHMAP, NEUROPHOTOMETRICS_LED_STATES
 from ibllib.pipes.photometry_tasks import FibrePhotometryPreprocess as PhotometryPreprocess
 from ibllib.io import raw_daq_loaders
 from ibllib.qc.base import QC
 from ibllib.pipes.training_preprocessing import (
     TrainingRegisterRaw, TrainingAudio, TrainingTrials, TrainingDLC, TrainingStatus, TrainingVideoCompress)
 from scipy import interpolate
-
-NEUROPHOTOMETRICS_LED_STATES = {
-    'Condition': {
-        0: 'No additional signal',
-        1: 'Output 1 signal HIGH',
-        2: 'Output 0 signal HIGH',
-        3: 'Stimulation ON',
-        4: 'GPIO Line 2 HIGH',
-        5: 'GPIO Line 3 HIGH',
-        6: 'Input 1 HIGH',
-        7: 'Input 0 HIGH',
-        8: 'Output 0 signal HIGH + Stimulation',
-        9: 'Output 0 signal HIGH + Input 0 signal HIGH',
-        10: 'Input 0 signal HIGH + Stimulation',
-        11: 'Output 0 HIGH + Input 0 HIGH + Stimulation',
-    },
-    'No LED ON': {0: 0, 1: 8, 2: 16, 3: 32, 4: 64, 5: 128, 6: 256, 7: 512, 8: 48, 9: 528, 10: 544, 11: 560},
-    'L415':  {0: 1, 1: 9, 2: 17, 3: 33, 4: 65, 5: 129, 6: 257, 7: 513, 8: 49, 9: 529, 10: 545, 11: 561},
-    'L470':  {0: 2, 1: 10, 2: 18, 3: 34, 4: 66, 5: 130, 6: 258, 7: 514, 8: 50, 9: 530, 10: 546, 11: 562},
-    'L560':  {0: 4, 1: 12, 2: 20, 3: 36, 4: 68, 5: 132, 6: 260, 7: 516, 8: 52, 9: 532, 10: 548, 11: 564}
-}
 
 CHANNELS = pd.DataFrame.from_dict(NEUROPHOTOMETRICS_LED_STATES)
 
@@ -233,7 +212,7 @@ class FibrePhotometry(BaseFibrePhotometry):
             A 1D array of timestamps, one per frame
         """
         chmap = kwargs.get('chmap', DAQ_CHMAP)
-        daq_data = raw_daq_loaders.load_channels_tdms(self.session_path / 'raw_photometry_data', chmap)
+        daq_data = raw_daq_loaders.load_channels_tdms(self.session_path.joinpath(self.collection), chmap=chmap)[0]
         trials_data = alfio.load_object(self.session_path / 'alf', 'trials')
         return self.sync_timestamps(daq_data, fp_data, trials_data)
 
@@ -332,7 +311,7 @@ class FibrePhotometry(BaseFibrePhotometry):
 class FibrePhotometryPreprocess(PhotometryPreprocess):
 
     def _run(self, **kwargs):
-        _, out_files = FibrePhotometry(self.session_path, collection=self.collection).extract(
+        _, out_files = FibrePhotometry(self.session_path, collection=self.device_collection).extract(
             regions=self.regions, path_out=self.session_path.joinpath('alf', 'photometry'), save=True)
         return out_files
 
