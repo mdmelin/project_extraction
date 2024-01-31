d81189b2bed646a2bc53d3080a99e716ffb86add
Miles Wells <k1o0@5tk.co>
2024-01-31 15:48:20 +0200

    Documentation; cuedBiased task QC and pipeline tasks

--- projects/extraction_tasks.py
+++ projects/extraction_tasks.py
@@ -1,1 +1,9 @@
+"""
+Custom behaviour extractor tasks for personal projects.
+
+The task class name(s) should be added to the
+
+NB: This may need changing in the future if one of these modules requires optional dependencies.
+"""
 from projects.neuromodulators import ChoiceWorldNeuromodulators
+from projects.samuel_cuedBiasedChoiceWorld import CuedBiasedTrials, CuedBiasedTrialsTimeline
