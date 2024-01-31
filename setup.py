e61d0f653b938dc39a5fb15f3e8cf64cf7d8ca08
Miles Wells <k1o0@5tk.co>
2024-01-31 18:49:16 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='project_extraction',
-    version='0.1.0',
+    version='0.2.0',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     description='Custom extractors for satellite tasks',
     license="MIT",
