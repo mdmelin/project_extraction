315f09c0a5c207ba6ef4af7e67536e27628394db
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2024-07-15 14:18:41 +0100

    add laserIntervals file

--- projects/alejandro_FPLROptoChoiceWorld.py
+++ projects/alejandro_FPLROptoChoiceWorld.py
@@ -11,8 +11,8 @@ from ibllib.io.extractors.bpod_trials import BiasedTrials
 
 
 class TrialsFPLROpto(BaseBpodTrialsExtractor):
-    var_names = BiasedTrials.var_names + ('laser_stimulation',)
-    save_names = BiasedTrials.save_names + ('_ibl_trials.laserStimulation.npy',)
+    var_names = BiasedTrials.var_names + ('laser_stimulation', 'laser_intervals',)
+    save_names = BiasedTrials.save_names + ('_ibl_trials.laserStimulation.npy', '_ibl_trials.laserIntervals.npy',)
 
     def _extract(self, extractor_classes=None, **kwargs) -> dict:
 
@@ -23,11 +23,18 @@ class TrialsFPLROpto(BaseBpodTrialsExtractor):
 
         # Extract laser stimulation dataset
         laser_stimulation = np.zeros_like(out['table']['contrastLeft'])
+        laser_intervals = np.full((out['table']['contrastLeft'].size, 2), np.nan)
         if 'FPR' in self.settings['PYBPOD_PROTOCOL']:
-            laser_stimulation[~np.isnan(out['table']['contrastRight'])] = 1
+            idx = ~np.isnan(out['table']['contrastRight'])
         elif 'FPL' in self.settings['PYBPOD_PROTOCOL']:
-            laser_stimulation[~np.isnan(out['table']['contrastLeft'])] = 1
+            idx = ~np.isnan(out['table']['contrastLeft'])
+
+        laser_stimulation[idx] = 1
+        # Laser stimulated at goCue for 200 ms
+        laser_intervals[idx, 0] = out['table']['goCue_times'][idx]
+        laser_intervals[idx, 1] = out['table']['goCue_times'][idx] + 200 / 1e3
 
         out['laser_stimulation'] = laser_stimulation
+        out['laser_intervals'] = laser_intervals
 
         return {k: out[k] for k in self.var_names}  # Ensures all datasets present and ordered
